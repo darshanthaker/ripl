3018a9cd744c8bfb57f65f88f79558ca68305639
darshan <darshan@ganesh-VirtualBox.(none)>
2014-07-30 13:15:46 -0700

    Added Mininet FatTreeTopo1 to list of known topos

--- ripl/mn.py
+++ ripl/mn.py
@@ -8,8 +8,10 @@ To use this file to run a RipL-specific topology on Mininet.  Example:
 """
 
 from ripl.dctopo import FatTreeTopo #, VL2Topo, TreeTopo
+from mininet.topolib import FatTreeTopo1
 
-topos = { 'ft': FatTreeTopo}
+topos = { 'ft': FatTreeTopo,
+          'fattree': FatTreeTopo1}
 #,
 #          'vl2': VL2Topo,
 #          'tree': TreeTopo }
